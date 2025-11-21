3a06f9574b9621d36ff6ae397c0784bf1e070c99
Anton Agaronov <144243055+AntonAgaronov@users.noreply.github.com>
2025-11-21 14:55:03 +0400

    init: peduchat init

---
+++ app/layouts/default.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1>PEDUCHAT!</h1>
+    <slot />
+  </div>
+</template>
+
+<script setup lang="ts">
+useHead({
+  title: 'Video Chat',
+  meta: [
+    { name: 'viewport', content: 'width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' },
+    { name: 'theme-color', content: '#0f172a' },
+  ],
+});
+</script>
+
+<style scoped></style>
